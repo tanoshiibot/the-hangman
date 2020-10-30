06661f6f722eff038d58a77821b98d9a876ce41a
tanoshiibot <fulljelly@outlook.fr>
2020-10-30 12:11:36 +0100

    début du filter sur une array de mots exemples. Je dois trouver un moyen de détecter les strings de chiffres facilement

--- hangman.js
+++ hangman.js
@@ -1,3 +1,23 @@
 window.onload = function() {
 
+    let words = ["moka", "tiramisu", "panna cotta", "caramel", "capucinno", "crêpe", "latte machiatto", "mille-feuille", "101010aaaaa"]
+    console.log(words);
+    words = words.filter(element => (element.length >= 5) && (![...element].includes(" ")));
+    console.log(words);
+    
+    //HTML
+
+    document.getElementById("test").addEventListener("click", () => {
+        let word = words[Math.floor(words.length * Math.random())];
+        word = [...word];
+        console.log(word);
+    })
+    
+    document.addEventListener("keydown", event => {
+        if ((event.keyCode >= 65) && (event.keyCode <= 90)) {
+            document.getElementById("output").innerText = event.keyCode;
+        }
+    })
+    
+
 };
