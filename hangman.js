0a127deb087f0b302853ef61ee049a3d9a6f52be
tanoshiibot <fulljelly@outlook.fr>
2020-10-30 16:02:26 +0100

    missChara pour détecter la victoire

--- hangman.js
+++ hangman.js
@@ -9,43 +9,55 @@ window.onload = function() {
     let spaces = [];
     let keysPressed = [];
     let lives = 9;
+    let missChara = 0;
 
     //HTML
 
-    document.getElementById("debug").innerText = `${lives} left before death !`
-    document.getElementById("test").addEventListener("click", () => {
-        word = [...words[Math.floor(words.length * Math.random())]];
-        console.log(word);
-        spaces = [...word].map(x => x == "-" ? " - " : " _ ");
-        spaces.forEach((x) => {
-            let a = document.createElement("LI");
-            a.innerText = x;
-            a.setAttribute("class", "character")
-            document.getElementById("word").appendChild(a);
-        })
+    document.getElementById("debug").innerText = `${lives} left before death !`;
+    lives = 9;
+    word = [...words[Math.floor(words.length * Math.random())]];
+    console.log(word);
+    spaces = [...word].map(x => x == "-" ? " - " : " _ ");
+    missChara = word.length;
+    spaces.forEach((x) => {
+        let a = document.createElement("LI");
+        a.innerText = x;
+        a.setAttribute("class", "character");
+        document.getElementById("word").appendChild(a);
+        if (x == " - ") {
+            missChara--;
+        } 
     })
-    
+
     document.addEventListener("keydown", event => {
-        if ((event.keyCode >= 65) && (event.keyCode <= 90)) {
-            document.getElementById("output").innerText = event.key;
-            if (keysPressed.includes(event.key)){
-                document.getElementById("output2").innerText = "déjà dedans !!!"
-            } else {
-                keysPressed.push(event.key);
-                document.getElementById("output2").innerText = keysPressed;
-                if (word.includes(event.key)) {
-                    word.forEach((x, i) => {
-                        if (x == event.key) {
-                            document.getElementsByClassName("character")[i].innerText = ` ${event.key} `;
-                        }
-                    })
+        if ((lives > 0) && (missChara > 0)) {
+            if ((event.keyCode >= 65) && (event.keyCode <= 90)) {
+                document.getElementById("output").innerText = event.key;
+                if (keysPressed.includes(event.key)){
+                    document.getElementById("output2").innerText = "déjà dedans !!!";
                 } else {
-                    lives --
-                    document.getElementById("hangman").setAttribute("src", `./img/${-lives + 9}.png`)
+                    keysPressed.push(event.key);
+                    if (word.includes(event.key)) {
+                        word.forEach((x, i) => {
+                            if (x == event.key) {
+                                document.getElementsByClassName("character")[i].innerText = ` ${event.key} `;
+                                missChara--;
+                                document.getElementById("output2").innerText = missChara;
+                                if (missChara == 0) {
+                                    document.getElementById("output").innerText = "your won :)";
+                                } 
+                            }
+                        })
+                    } else {
+                        lives--;
+                        document.getElementById("hangman").setAttribute("src", `./img/${-lives + 9}.png`);
+                    }
                 }
             }
+            document.getElementById("debug").innerText = `${lives} left before death !`
+        } else if (lives == 0) {
+            document.getElementById("output").innerText = "your ded :(";
         }
-        document.getElementById("debug").innerText = `${lives} left before death !`
     })
 
 };
