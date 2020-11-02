5471e972517b2f1a76bff15cf5ed11cc6bcdada4
tanoshiibot <fulljelly@outlook.fr>
2020-11-02 10:32:25 +0100

    function accentsTidy trouvée sur stackoverflow

--- hangman.js
+++ hangman.js
@@ -11,11 +11,32 @@ window.onload = function() {
     let lives = 9;
     let missChara = 0;
 
-    //HTML
 
+    function accentsTidy(s) {
+        let r = s.toLowerCase();
+        r = r.replace(new RegExp(/\s/g),"");
+        r = r.replace(new RegExp(/[àáâãäå]/g),"a");
+        r = r.replace(new RegExp(/æ/g),"ae");
+        r = r.replace(new RegExp(/ç/g),"c");
+        r = r.replace(new RegExp(/[èéêë]/g),"e");
+        r = r.replace(new RegExp(/[ìíîï]/g),"i");
+        r = r.replace(new RegExp(/ñ/g),"n");                
+        r = r.replace(new RegExp(/[òóôõö]/g),"o");
+        r = r.replace(new RegExp(/œ/g),"oe");
+        r = r.replace(new RegExp(/[ùúûü]/g),"u");
+        r = r.replace(new RegExp(/[ýÿ]/g),"y");
+        r = r.replace(new RegExp(/\W/g),"");
+        return r;
+    };
+    
+    //HTML
+    
     document.getElementById("debug").innerText = `${lives} left before death !`;
     lives = 9;
     word = [...words[Math.floor(words.length * Math.random())]];
+    word.forEach((x, i) => {
+        word[i] = accentsTidy(x);
+    })
     console.log(word);
     spaces = [...word].map(x => x == "-" ? " - " : " _ ");
     missChara = word.length;
